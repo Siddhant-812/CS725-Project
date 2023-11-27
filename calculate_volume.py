0eaaffe36bbad4b55dc79884abb4a2e4b264af90
Siddhant-812 <siddhant8057@gmail.com>
2023-11-28 01:26:41 +0530

    Test result visualiser

--- calculate_volume.py
+++ calculate_volume.py
@@ -2,9 +2,14 @@ import pandas as pd
 import math
 
 cls_name = { "apple" : "ellipsoid", "banana" : "irregular", "bread" : "column",  "bun" : "irregular", "coin" : "",
-            "doughnut" : "irregular", "egg" : "ellipsoid", "lemon" : "ellipsoid", "mongo" : "irregular",
-            "orange" : "ellipsoid", "pear" : "irregular", "plum" : "ellipsoid", "qiwi" : "ellipsoid",
+            "doughnut" : "irregular", "egg" : "ellipsoid", "fired_dough_twist":"irregular", "grape":"irregular", "lemon" : "ellipsoid", "mango" : "irregular",
+            "litchi":"irregular", "mooncake":"column", "orange" : "ellipsoid", "pear" : "irregular", "peach":"ellipsoid", "plum" : "ellipsoid", "qiwi" : "ellipsoid",
             "sachima" : "column", "tomato" : "ellipsoid" }
+
+density_energy = { "apple" : [0.78,0.52], "banana" : [0.91,0.89], "bread" : [0.18,3.15],  "bun" : [0.34,2.23], 
+            "doughnut" : [0.31,4.34], "egg" : [1.03,1.43], "fired_dough_twist":[0.58,24.16], "grape":[0.97,0.69],
+            "lemon" : [0.96, 0.29], "litchi":[1.00,0.66],"mango" : [1.07,0.60], "mooncake": [0.96, 18.83], "orange" : [0.90,0.63], "pear" : [1.02,0.39],
+            "peach":[0.96,0.57], "plum" : [1.01,0.46], "qiwi" : [0.97,0.61], "sachima" : [0.22,21.45], "tomato" : [0.98,0.27] }
 
 
 sideview_data = pd.read_csv("sideview_pixels.csv")  #[Bounding Box Objects,Total Pixel Count,Row Pixel Count Sum,Max Row Pixel Count,Bounding Box (H + W)]
@@ -32,6 +37,9 @@ elif(object_shape == "column"):
 elif(object_shape == "irregular"):
      volume = object_topview[1]*math.pow(alpha_top,2)*alpha_side*object_sideview[2]/(object_sideview[2]**2)
 
-print(f"Estimated Volume for {object_name} is: {volume}")
+calories = volume*density_energy[object_name][0]*density_energy[object_name][1]
+
+print(f"Estimated Volume for {object_name} is: {volume} mm³")
+print(f"Calorie estimate for {object_name} is: {calories} Kcal")
      
 
---
+++ visualise.py
@@ -0,0 +1,50 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Sample data
+data = {'Apple': -6.9, 'Banana': 57.9, 'Bread': 79.4, 'Bun': 15.2, 'Doughnut': 18.6, 'Egg': 17.1, 'Fried Dough Twist': 11.8, 
+        'Grape': 143.8, 'Lemon': 0.61, 'Litchi': 20.3, 'Mango': 12.5, 'Mooncake': 13.3, 'Orange': 2.8, 'Pear': 20.9,
+        'Peach': 4.7, 'Plum': -3.6, 'Qiwi': 4.4, 'Sachima': 20.5, 'Tomato': 1.5}
+
+# Extract labels and values
+labels = list(data.keys())
+values = list(data.values())
+
+# Plotting
+fig, ax = plt.subplots()
+
+# Plot the bars with colors based on the sign of x values, reduce bar width
+bars = ax.barh(labels, values, color=np.where(np.array(values) > 0, 'tab:blue', 'tab:blue'), height=0.6)
+
+# Add x value labels on top of each bar with adjusted position and percent sign
+for bar, value in zip(bars, values):
+    label_position = (min(value, 40), bar.get_y() + bar.get_height() / 2)
+    ax.text(*label_position, f'{value:.1f}%', ha='left' if value > 0 else 'right',
+            va='center', color='black', fontweight='bold', fontsize=8)
+
+# Add labels and title
+ax.set_xlabel('Mean Error (%)')
+#ax.set_ylabel('Class Labels')
+#ax.set_title('Bar Plot with Positive and Negative Values')
+
+# Adjust x-axis limits for equal spacing on both sides
+x_limit = 40
+ax.set_xlim(left=-x_limit, right=x_limit)
+
+# Set x-axis ticks with the specified interval
+x_interval = 10
+ax.set_xticks(np.arange(-x_limit, x_limit + 1, x_interval))
+
+# Remove y-axis ticks
+ax.tick_params(axis='y', which='both', left=False)
+
+# Remove vertical dotted lines for each x value
+ax.xaxis.grid(False)
+
+# Add vertical lines to x-axis labels
+for x_tick in ax.get_xticks():
+    ax.axvline(x=x_tick, color='gray', linestyle='-', linewidth=0.5)
+
+
+# Show the plot
+plt.show()
